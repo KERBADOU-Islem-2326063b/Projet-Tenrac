e87340d385d6a0d09a744fb7cab9a1df4393d608
KERBADOU Islem <islem.kerbadou@etu.univ-amu.fr>
2024-09-10 15:20:13 +0200

    feat: latest version

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
-<?php
-    echo '<strong> Voici mon premier programme PHP </strong><br>' . "\n";
-?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <style>
+            <?php include '_assets/styles/index.css';?>
+        </style>
+        <meta charset="UTF-8">
+        <meta name="description" content="Tenrac, adeptes des tenders de poulet à la raclette">
+        <meta name="keywords" content="Tenrac, tenders, poulet, raclette">
+        <meta name="author" content="KERBADOU Islem, ODERZO Flavio, TRAN Thomas, ALVARES Titouan, Avias Daphne">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tenrac</title>
+    </head>
+    <body>
+    <header>
+        <img src="https://i.imgur.com/e1rxMEL.png" width="1em" height="1em">
+        <img src="https://i.imgur.com/FR6znMh.png" width="2em" height="1em">
+        <ul>
+            <li><a href="#">REPAS</a></li>
+            <li><a href="#">PLATS</a></li>
+            <li><a href="#">AUTHENTIFICATION</a></li>
+        </ul>
+    </header>
+    <main>Ouais le main</main>
+    <footer>Les raclettes</footer>
+    </body>
+</html>
