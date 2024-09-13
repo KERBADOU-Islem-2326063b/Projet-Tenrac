e74d3f8b58c2e8280204850f7f38700e8dd3643e
KERBADOU Islem <islem.kerbadou@etu.univ-amu.fr>
2024-09-13 15:12:26 +0200

    feat:  dernière version

--- index.php
+++ index.php
@@ -7,21 +7,53 @@
         <meta charset="UTF-8">
         <meta name="description" content="Tenrac, adeptes des tenders de poulet à la raclette">
         <meta name="keywords" content="Tenrac, tenders, poulet, raclette">
-        <meta name="author" content="KERBADOU Islem, ODERZO Flavio, TRAN Thomas, ALVARES Titouan, Avias Daphne">
+        <meta name="author" content="KERBADOU Islem, ODERZO Flavio, TRAN Thomas, ALVARES Titouan, AVIAS Daphné">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Tenrac</title>
     </head>
     <body>
     <header>
-        <img src="https://i.imgur.com/e1rxMEL.png" width="1em" height="1em">
-        <img src="https://i.imgur.com/FR6znMh.png" width="2em" height="1em">
-        <ul>
-            <li><a href="#">REPAS</a></li>
-            <li><a href="#">PLATS</a></li>
-            <li><a href="#">AUTHENTIFICATION</a></li>
-        </ul>
+        <a href="#"><img src="https://i.imgur.com/e1rxMEL.png" width="40px" height="40px" class="image"></a>
+        <img src="https://i.imgur.com/FR6znMh.png" width="15%" height="80%" class="logo">
+        <nav>
+            <ul id = "menu">
+                <li class="li-header"><a href="#">REPAS</a></li>
+                <li class="li-header"><a href="#">PLATS</a></li>
+                <li class="li-header"><a href="#">AUTHENTIFICATION</a></li>
+                <li class="li-header"><a href="#">L'ORDRE</a></li>
+            </ul>
+        </nav>
     </header>
-    <main>Ouais le main</main>
-    <footer>Les raclettes</footer>
+
+    <main>taset</main>
+
+
+
+
+
+    <footer>
+        <div>
+            <h2>A propos de nous :</h2>
+            <ul>
+                <li>
+                    <h3>Adresse :</h3>
+                    <p>105 rue de la raclette, 1934 Bagnes </p>
+                </li>
+                <li>
+                    <h3>Email :</h3>
+                    <p>tenrac@poulet.fr</p>
+                </li>
+                <li>
+                    <h3>Téléphone :</h3>
+                    <p>06.12.34.56.78</p>
+                </li>
+            </ul>
+        </div>
+
+
+        <p>© Copyright 2024 Tenrac - All Rights Reserved.</p>
+    </footer>
+
+
     </body>
 </html>
