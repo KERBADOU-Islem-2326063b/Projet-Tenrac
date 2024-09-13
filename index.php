f736586e8488635da552f01cd422ca9deab095ee
KERBADOU Islem <islem.kerbadou@etu.univ-amu.fr>
2024-09-13 17:21:04 +0200

    feat: ajout layout

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
         <style>
             <?php include '_assets/styles/index.css';?>
         </style>
+        <script>
+            <?php require_once("_assets/javascript/index.js");?>
+        </script>
         <meta charset="UTF-8">
         <meta name="description" content="Tenrac, adeptes des tenders de poulet à la raclette">
         <meta name="keywords" content="Tenrac, tenders, poulet, raclette">
@@ -13,45 +16,96 @@
     </head>
     <body>
     <header>
-        <a href="#"><img src="https://i.imgur.com/e1rxMEL.png" width="40px" height="40px" class="image"></a>
-        <img src="https://i.imgur.com/FR6znMh.png" width="15%" height="80%" class="logo">
-        <nav>
-            <ul id = "menu">
+        <div class="header-left">
+            <div id="mySidenav" class="sidenav">
+                <a id="closeBtn" href="#" class="close">×</a>
+                <ul id = "menu">
+                    <li class="li-header"><a href="#">REPAS</a></li>
+                    <li class="li-header"><a href="#">PLATS</a></li>
+                    <li class="li-header"><a href="#">CONNEXION</a></li>
+                    <li class="li-header"><a href="#">L'ORDRE</a></li>
+                </ul>
+            </div>
+
+            <a href="#" id="openBtn">
+          <span class="burger-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+            </a>
+            <img src="https://i.imgur.com/FR6znMh.png" width="200px" height="80%" class="logo">
+            <ul class="menu">
                 <li class="li-header"><a href="#">REPAS</a></li>
                 <li class="li-header"><a href="#">PLATS</a></li>
-                <li class="li-header"><a href="#">AUTHENTIFICATION</a></li>
+                <li class="li-header"><a href="#">CONNEXION</a></li>
                 <li class="li-header"><a href="#">L'ORDRE</a></li>
             </ul>
-        </nav>
+        </div>
+        <div class = "header-right">
+            <a href="#"><img src="https://i.imgur.com/Uw4eL5a.png" width="40px" height="38px" class="logo"></a>
+        </div>
+
+
     </header>
 
-    <main>taset</main>
+
+
+    <main>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+        <p>f</p>
+    </main>
 
 
 
 
 
     <footer>
-        <div>
-            <h2>A propos de nous :</h2>
-            <ul>
-                <li>
-                    <h3>Adresse :</h3>
-                    <p>105 rue de la raclette, 1934 Bagnes </p>
-                </li>
-                <li>
-                    <h3>Email :</h3>
-                    <p>tenrac@poulet.fr</p>
-                </li>
-                <li>
-                    <h3>Téléphone :</h3>
-                    <p>06.12.34.56.78</p>
-                </li>
-            </ul>
+        <div class="apropos">
+            <p><strong>Adresse :</strong> 105 rue de la raclette, 1934 Bagnes, Suisse. </p>
+            <p><strong>Email :</strong> tenrac@poulet.fr</p>
+            <p><strong>Téléphone :</strong> 06.12.34.56.78</p>
+            <div>
+                <a href="#"><img src="https://imgur.com/0kZm59H.png" width="30px" height="60%"></a>
+                <a href="#"><img src="https://imgur.com/CTTXkU7.png" width="30px" height="60%"></a>
+                <a href="#"><img src="https://imgur.com/QwF9yiJ.png" width="30px" height="60%"></a>
+            </div>
         </div>
 
 
-        <p>© Copyright 2024 Tenrac - All Rights Reserved.</p>
+        <div>
+            <p class="copyright">© Copyright 2024 Tenrac - All Rights Reserved.</p>
+        </div>
+
+
+
     </footer>
 
 
