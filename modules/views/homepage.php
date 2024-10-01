2f2a3f331c02b7da1f7f9e0666a89d89e7828a1a
KERBADOU-Islem-2326063b <islem.kerbadou@etu.univ-amu.fr>
2024-10-01 22:06:41 +0200

    feat: ajout de texte dans la homepage. Modifications legeres.

--- modules/views/homepage.php
+++ modules/views/homepage.php
@@ -5,22 +5,25 @@ class Homepage {
     public function showView() {
         ?>
         <main>
+            <h1>Bienvenue cher voyageur</h1>
+            <p>Tenrac a pour but de réunir des adeptes de tenders de poulet à la raclette. Un tenrac est avant tout loyal à ces activités. Apprennez la vie, goûtez avec nous aux différentes recettes de poulet à la raclette.</p>
+            <p>Sur ce site, vous pourrez rejoindre un <a href="/clubs">club</a> et voir leurs informations, voir la liste des <a href="/repas">repas</a> et plats ainsi que leurs ingrédiants, et plus encore !</p>
             <div class="carousel">
                 <div class="carousel-container">
                     <div class="carousel-slide">
-                        <img src="https://www.darty.com/darty-et-vous/sites/default/files/2022-07/raclette_2021_494.jpg" width="450" height="300" alt="Raclette miam">
+                        <img src="https://www.darty.com/darty-et-vous/sites/default/files/2022-07/raclette_2021_494.jpg" width="450" height="300" alt="Raclette">
                         <p>Une raclette, et du fromage fondu ...</p>
                     </div>
                     <div class="carousel-slide">
-                        <img src="https://charcuterie-d-antan.fr/images/virtuemart/articles/tenders2.jpg" width="450" height="300" alt="Tenders miam 1">
+                        <img src="https://charcuterie-d-antan.fr/images/virtuemart/articles/tenders2.jpg" width="450" height="300" alt="Tenders">
                         <p>Un poulet bien croustillant</p>
                     </div>
                     <div class="carousel-slide">
-                        <img src="https://www.darty.com/darty-et-vous/sites/default/files/2022-07/raclette_2021_494.jpg" width="450" height="100" alt="Image d'un signe egal">
-                        <p>Du fromage bien fondu</p>
+                        <img src="https://www.darty.com/darty-et-vous/sites/default/files/2022-07/raclette_2021_494.jpg" width="450" height="100" alt="Raclette">
+                        <p>Une raclette, et du fromage fondu ...</p>
                     </div>
                     <div class="carousel-slide">
-                        <img src="https://charcuterie-d-antan.fr/images/virtuemart/articles/tenders2.jpg" width="450" height="300" alt="Image 1">
+                        <img src="https://charcuterie-d-antan.fr/images/virtuemart/articles/tenders2.jpg" width="450" height="300" alt="Tenders">
                         <p>Un poulet bien croustillant</p>
                     </div>
                 </div>
@@ -29,8 +32,7 @@ class Homepage {
                 <button class="prev" id="prevBtn">&#10094;</button>
                 <button class="next" id="nextBtn">&#10095;</button>
             </div>
-            <p>Un tenrac est avant tout loyal à ces activités. Apprennez la vie, goûtez avec nous aux différentes recettes de poulet à la raclette. Du fromage, et la meilleure viande au monde ...</p>
-            <p>Qu'attendez vous, cliquez <a href="/login">ici</a> !</p>
+            <p> Du fromage, et la meilleure viande au monde ... Qu'attendez vous,  <a href="/login">rejoingez nous !</a></p>
         </main>
         <?php
     }
